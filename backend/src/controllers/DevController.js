94b8b6d304fd266ff47c1e4bba21dbdabb9dd4f1
Maria <nathyellemonteiro19@gmail.com>
2020-02-27 10:02:16 -0300

    created SearchController

--- backend/src/controllers/DevController.js
+++ backend/src/controllers/DevController.js
@@ -1,31 +1,40 @@
 const axios = require('axios');
 const Dev = require('../models/Dev');
+const parseStringAsArray = require('../utils/parseStringAsArray');
 
 module.exports = {
+    async index(req, res) {
+        const devs = await Dev.find();
+
+        return res.json(devs);
+    },
+
     async store(req, res) {
         const { github_username, techs, latitude, longitude } = req.body;
 
-        const dev = await Dev.findOne({ gitResponse });
+        let dev = await Dev.findOne({ github_username });
+
+        if(!dev) {
+            const gitResponse = await axios.get(`https://api.github.com/users/${github_username}`);
     
-        const gitResponse = await axios.get(`https://api.github.com/users/${github_username}`);
-    
-        const { name = login, avatar_url, bio } = gitResponse.data;
-    
-        const techsArray = techs.split(',').map(tech => tech.trim());
-    
-        const location = {
-            type: 'Point',
-            coordinates: [longitude, latitude]
+            const { name = login, avatar_url, bio } = gitResponse.data;
+        
+            const techsArray = parseStringAsArray(techs);
+        
+            const location = {
+                type: 'Point',
+                coordinates: [longitude, latitude]
+            }
+        
+            dev = await Dev.create({
+                github_username,
+                name,
+                avatar_url,
+                bio,
+                techs: techsArray,
+                location
+            });
         }
-    
-        const dev = await Dev.create({
-            github_username,
-            name,
-            avatar_url,
-            bio,
-            techs: techsArray,
-            location
-        });
     
         return res.json(dev)
     }
